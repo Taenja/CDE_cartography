50bd06d312e0e2d523d868450a1aabc7dfc44f56
Tanya Singh <singh.tanya3298@gmail.com>
2020-11-04 20:25:36 +0100

    Initialized the project (First Commit)

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var map = L.map('map');
+map.setView([51.2, 7], 9);
+
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; OpenStreetMap contributors'
+}).addTo(map);
+
+var marker = L.marker([51.2, 7]).addTo(map);
+var sidebar = L.control.sidebar('sidebar').addTo(map);
